6108370c7e0ea83e439d1e91de70b484e1151d45
FabianCQ <102173023+FabianCQ@users.noreply.github.com>
2022-05-10 17:35:53 -0400

    Update codigo_arduino.ino

--- codigo_arduino/codigo_arduino.ino
+++ codigo_arduino/codigo_arduino.ino
@@ -1,33 +1,38 @@
-#include "notas.h"
+#define NOTA_SOL5   784
+#define NOTA_LA5    880
+#define NOTA_SI5    988
+#define NOTA_DO6   1047
+#define NOTA_RE6   1175
+
 int melodia[] = {
-  NOTA_SI4, NOTA_SI4, NOTA_SI4,
-  NOTA_SI4, NOTA_SI4, NOTA_SI4,
-  NOTA_SI4, NOTA_RE3, NOTA_SOL4, NOTA_LA4,
-  NOTA_SI4,
-  NOTA_DO3, NOTA_DO3, NOTA_DO3, NOTA_DO3,
-  NOTA_DO3, NOTA_SI4, NOTA_SI4, NOTA_SI4, NOTA_SI4,
-  NOTA_SI4, NOTA_LA4, NOTA_LA4, NOTA_SI4,
-  NOTA_LA4, NOTA_RE3
+  NOTA_SI5, NOTA_SI5, NOTA_SI5,
+  NOTA_SI5, NOTA_SI5, NOTA_SI5,
+  NOTA_SI5, NOTA_RE6, NOTA_SOL5, NOTA_LA5,
+  NOTA_SI5,
+  NOTA_DO6, NOTA_DO6, NOTA_DO6, NOTA_DO6,
+  NOTA_DO6, NOTA_SI5, NOTA_SI5, NOTA_SI5, NOTA_SI5,
+  NOTA_SI5, NOTA_LA5, NOTA_LA5, NOTA_SI5,
+  NOTA_LA5, NOTA_RE6
 };
 
 int duraciones[] = {
-  2, 2, 4,
-  2, 2, 4,
-  2, 2, 2, 2,
-  8,
-  2, 2, 3, 1,
-  2, 2, 2, 1, 1,
-  2, 2, 2, 2,
-  4, 4
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 4, 4,
+  1,
+  4, 4, 6, 8,
+  4, 4, 4, 8, 8,
+  4, 4, 4, 4,
+  2, 2
 };
 
-int multiplicadorDuracion = 4;
-float multiplicadorPausa = 1.3;
+int multiplicadorDuracion = 2;
+float multiplicadorPausa = 2;
 int pinParlante = 8;
 
 void setup() {
 
-for (int nota = 0; nota < 8 ; nota++) {
+  for (int nota = 0; nota < 26 ; nota++) {
 
     // duracion de la nota en milisegundos
     int duracionNota = 1000 / (duraciones[nota] * multiplicadorDuracion);
@@ -35,16 +40,31 @@ for (int nota = 0; nota < 8 ; nota++) {
     // tone(pin, frecuencia, duracion)
     tone(pinParlante, melodia[nota], duracionNota);
 
-    // pausa entre notas
-    int pausa = duracionNota * multiplicadorPausa;
+    // hay 5 notas
+    // cada nota prendera una luz distinta
 
-    // pausar para mantener nota
-    delay(pausa);
+    if (melodia[nota] == NOTA_DO6)  {
+      // prender luz roja
+    }  else if (melodia[nota] == NOTA_RE6)  {
+      // prender luz verde
+    }   else if (melodia[nota] == NOTA_RE6)  {
+      // prender luz verde
+    }   else if (melodia[nota] == NOTA_RE6)  {
+      // prender luz verde
+    }   else if (melodia[nota] == NOTA_RE6)  {
+      // prender luz verde
+    }
 
-    // silenciar nota
-    noTone(pinParlante);
+  // pausa entre notas
+  int pausa = duracionNota * multiplicadorPausa;
 
-  }
+  // pausar para mantener nota
+  delay(pausa);
+
+  // silenciar nota
+  noTone(pinParlante);
+
+}
 
 }
 
